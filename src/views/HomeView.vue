828c84dd03f3fb8b453b82ffbf808dfcca9bf9a8
Petr Flídr <petr.flidr@seznam.cz>
2022-07-10 23:27:32 +0200

    Added: date

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <script setup>
-import TheWelcome from '@/components/TheWelcome.vue'
+  import { ref } from "vue"
+  const today = new Date();
+  const date = ref(today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate());
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <h1>Home page {{date}}</h1>
   </main>
 </template>
